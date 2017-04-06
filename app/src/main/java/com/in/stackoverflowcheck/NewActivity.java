bba9f22e4429963c96987d8d3cad4923bb388cbf
nagullasravanthi <nagullasravanthi@gmail.com>
2017-04-06 16:33:06 +0530

    new Activity added

---
+++ app/src/main/java/com/in/stackoverflowcheck/NewActivity.java
@@ -0,0 +1,16 @@
+package com.in.stackoverflowcheck;
+
+import android.app.Activity;
+import android.app.Dialog;
+
+/**
+ * Created by sravanthi.boyina on 4/6/2017.
+ */
+
+public class NewActivity extends Activity {
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        return super.onCreateDialog(id);
+    }
+}
